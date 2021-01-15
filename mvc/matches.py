aae56664255b96ba87d4d2e4ef872c1c7989d66d
Josiney Junior <josineyjunior14@gmail.com>
2021-01-15 03:47:46 -0300

    updating matches and matchList

--- mvc/matches.py
+++ mvc/matches.py
@@ -2,11 +2,9 @@ import requests
 
 from APIkey import getApiKey
 
-
-def matches(id):
-    URL = "https://br1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + id + "?api_key=" + getApiKey()
+def matches(matchID):
+    URL = "https://br1.api.riotgames.com/lol/match/v4/matches/" + matchID + "?api_key=" + getApiKey()
     dados = requests.get(URL)
     return dados.json()
 
-
-print(matches('i0unkXaDiO3VL-oKzzAR2D9XUkx7nak2429kkPk1cFpoaYY'))
+print(matches('2160416202'))
